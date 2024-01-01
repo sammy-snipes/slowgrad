379012745bb21edce9c483ce7a6d4296c8e549d1
SammyCDTXT <hanzomain26267@gmail.com>
2024-01-01 09:28:02 -0500

    test for cross entropy loss

--- tests/test_models.py
+++ tests/test_models.py
@@ -1,7 +1,12 @@
-from slowgrad.models import SlowgradSequential, SlowgradMSELoss
+from slowgrad.models import (
+    SlowgradSequential,
+    SlowgradCrossEntropyLoss,
+    SlowgradMSELoss,
+)
 from slowgrad.engine import SlowgradVar
 import torch
 import torch.nn as nn
+import torch.nn.functional as F
 import unittest
 from einops import rearrange
 
@@ -41,6 +46,7 @@ class TestSequential(unittest.TestCase):
         torch_model(t).sum().backward()
         slowgrad_model(s).sum().backward()
 
+        self.assertTrue(torch.allclose(torch_model(t), slowgrad_model(s).data))
         self.assertTrue(
             all(
                 self.compare_params(
@@ -70,6 +76,45 @@ class TestSequential(unittest.TestCase):
         nn.MSELoss()(torch_model(t), a).backward()
         SlowgradMSELoss()(slowgrad_model(s), b).backward()
 
+        self.assertTrue(torch.allclose(torch_model(t), slowgrad_model(s).data))
+        self.assertTrue(
+            all(
+                self.compare_params(
+                    torch_model.parameters(), slowgrad_model.parameters()
+                )
+            )
+        )
+
+    def test_cross_entropy_loss_nn(self):
+        batch_size = 10
+        num_classes = 2
+        features = 5
+
+        x = torch.randn(batch_size, features, requires_grad=True)
+        y = F.one_hot(
+            torch.arange(batch_size) % num_classes, num_classes=num_classes
+        ).type(torch.float)
+
+        xs, ys = SlowgradVar(x.detach().clone()), SlowgradVar(y.detach().clone())
+
+        torch_model = nn.Sequential(
+            nn.Linear(5, 5, bias=True),
+            nn.Sigmoid(),
+            nn.Linear(5, 5, bias=True),
+            nn.Sigmoid(),
+            nn.Linear(5, 5, bias=False),
+            nn.Sigmoid(),
+            nn.Linear(5, 5, bias=False),
+            nn.Sigmoid(),
+            nn.Linear(5, 2),
+        )
+        slowgrad_model = SlowgradSequential().from_torch(torch_model)
+        torch_result = nn.CrossEntropyLoss()(torch_model(x), y)
+        slowgrad_result = SlowgradCrossEntropyLoss()(slowgrad_model(xs), ys)
+        torch_result.backward()
+        slowgrad_result.backward()
+
+        self.assertTrue(torch.allclose(torch_result, slowgrad_result.data))
         self.assertTrue(
             all(
                 self.compare_params(
